ff8f12da492ecb481506994c343946542d627b9d
caominghui <1241884874@qq.com>
2024-03-06 17:14:15 +0800

    4.1 配置Mysql与注册登录模块(上)

--- backend/src/main/java/com/kob/backend/BackendApplication.java
+++ backend/src/main/java/com/kob/backend/BackendApplication.java
@@ -1,12 +1,15 @@
 package com.kob.backend;
 
+import org.jetbrains.annotations.TestOnly;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @SpringBootApplication
 public class BackendApplication {
+
 
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
