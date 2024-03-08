6f9812664de72cf6289bf6a4709895ad66ecaae9
caominghui <1241884874@qq.com>
2024-03-08 13:31:03 +0800

    4.2 配置Mysql与注册登录模块

--- web/src/App.vue
+++ web/src/App.vue
@@ -11,6 +11,52 @@ import "bootstrap/dist/js/bootstrap"
 export default {
   components: {
     NavBar
+  },
+  setup(){
+    // $.ajax({
+    //   url: "http://127.0.0.1:3000/user/account/token/",
+    //   type: "post",
+    //   data: {
+    //     username: 'bb',
+    //     password: "pbb",
+    //   },
+    //   success(resp) {
+    //     console.log(resp);
+    //   },
+    //   error(resp) {
+    //     console.log(resp);
+    //   }
+    // });
+
+    // $.ajax({
+    //   url: "http://127.0.0.1:3000/user/account/info/",
+    //   type: "get",
+    //   headers: {
+    //     Authorization: "Bearer " + "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI3NWY1YTU1NjQ5NTY0MTAwOTA0ODVkOGFiZTQxMDRjYiIsInN1YiI6IjIiLCJpc3MiOiJzZyIsImlhdCI6MTcwOTgwMDc5NywiZXhwIjoxNzExMDEwMzk3fQ.hKxprcanGGbN3jwvtacjsXHbsOD7oJB-dAukk8u10xM"
+    //   },
+    //   success(resp) {
+    //     console.log(resp);
+    //   },
+    //   error(resp) {
+    //     console.log(resp);
+    //   }
+    // });
+
+    // $.ajax({
+    //   url:"http://127.0.0.1:3000/user/account/register/",
+    //   type: "post",
+    //   data: {
+    //     username: "yxc2",
+    //     password: "123",
+    //     confirmedPassword: "123",
+    //   },
+    //   success(resp) {
+    //     console.log(resp);
+    //   },
+    //   error(resp) {
+    //     console.log(resp);
+    //   }
+    // })
   }
 }
 </script>
